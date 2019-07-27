b5705e83b1eaf2ea6fc534e3b539fba40247b94f
Martin Steinegger <themartinsteinegger@gmail.com>
2019-07-27 19:43:11 -0400

    Add createststs

--- src/commons/LocalCommandDeclarations.h
+++ src/commons/LocalCommandDeclarations.h
@@ -7,5 +7,5 @@ extern int distanceton(int argc, const char** argv, const Command &command);
 extern int conterminator(int argc, const char** argv, const Command &command);
 extern int multipletaxas(int argc, const char** argv, const Command &command);
 extern int extractalignments(int argc, const char** argv, const Command &command);
-
+extern int createstats(int argc, const char** argv, const Command &command);
 #endif
